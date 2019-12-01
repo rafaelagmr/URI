9c5138d2bd6aedf053ac5c48d8d3312dfc806cb5
rafaelagmr <rafaelagmaciel12@gmail.com>
2019-11-30 22:50:20 -0300

    First commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a, b, x; 
+	scanf("%d", &a);
+	scanf("%d", &b);
+	x=a+b; 
+
+   // printf() displays the string inside quotation
+   printf("X = %d\n", x);
+
+}
